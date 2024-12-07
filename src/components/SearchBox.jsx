04dd1e00b4ae2312fe513f4caa4feea531fa81f3
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-07 21:32:25 +0100

    add

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFilter } from "..//components/filtersSlice";
+import "./SearchBox.css";
+
+const SearchBox = () => {
+  const filter = useSelector((state) => state.filters.name);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <div>
+      <label>
+        Find contacts by name
+        <input type="text" value={filter} onChange={handleChange} />
+      </label>
+    </div>
+  );
+};
+
+export default SearchBox;
