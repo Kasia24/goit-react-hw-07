c5f8819e55829a8f038500a8d93368824d5375d7
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-08 13:54:00 +0100

    add

--- src/components/redux/filtersSlice.js
+++ src/components/redux/filtersSlice.js
@@ -1,24 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  name: "", // Zainicjowanie 'name' na pusty string
-};
-
 const filtersSlice = createSlice({
   name: "filters",
   initialState: {
     name: "",
-    number: "",
   },
   reducers: {
     setNameFilter: (state, action) => {
       state.name = action.payload;
     },
-    setNumberFilter: (state, action) => {
-      state.number = action.payload;
-    },
   },
 });
 
-export const { setNameFilter, setNumberFilter } = filtersSlice.actions;
+export const { setNameFilter } = filtersSlice.actions;
 export default filtersSlice.reducer;
