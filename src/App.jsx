6a9528eac68e32de385ae8127346c3ac57e82e4e
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-08 11:29:34 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { fetchContacts } from "./components/contactsSlice";
+import { fetchContacts } from "./components/redux/contactsSlice";
 import ContactForm from "./components/ContactForm";
 import SearchBox from "./components/SearchBox";
 import ContactList from "./components/ContactList";
