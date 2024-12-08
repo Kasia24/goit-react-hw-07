b57b617d72fc0e0f4cf53900c4cf1f858ba5a9a3
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-08 13:57:51 +0100

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react"; // Jeśli używasz redux-persist
+import { store, persistor } from "./redux/store"; // Zainicjowanie store i persistor z redux-persist
 import { useDispatch } from "react-redux";
-import { fetchContacts } from "./redux/contactsSlice";
-import ContactForm from "./ContactForm";
-import SearchBox from "./SearchBox";
-import ContactList from "./ContactList";
+import { fetchContacts } from "./redux/contactsSlice"; // Akcja do pobierania kontaktów
+import ContactForm from "./ContactForm"; // Formularz do dodawania kontaktów
+import SearchBox from "./SearchBox"; // Pole wyszukiwania kontaktów
+import ContactList from "./ContactList"; // Lista kontaktów
 
+// Komponent FetchContacts, który wywołuje akcję fetchContacts
 const FetchContacts = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(fetchContacts()); // Wywołanie akcji do pobrania danych z API
   }, [dispatch]);
 
-  return null; // Ten komponent nie renderuje niczego wizualnego
+  return null; // Ten komponent nie renderuje niczego, tylko wykonuje efekt uboczny
 };
+
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        {" "}
+        {/* Jeśli używasz redux-persist */}
         <div>
           <h1>Phonebook</h1>
-          <FetchContacts /> {/* Tylko jeden mechanizm pobierania danych */}
+          <FetchContacts />{" "}
+          {/* Komponent FetchContacts uruchamia akcję fetchContacts */}
           <ContactForm />
           <SearchBox />
           <ContactList />
