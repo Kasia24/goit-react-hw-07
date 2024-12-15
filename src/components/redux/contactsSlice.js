23b53daf7b61d9b44ca41c865c5d7a559849e689
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-15 13:20:05 +0100

    add

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = "https://connections-api.goit.global";
+const BASE_URL = "https://connections-api.goit.global/docs/#/";
 
 // Akcja do pobierania kontaktów
 export const fetchContacts = createAsyncThunk(
