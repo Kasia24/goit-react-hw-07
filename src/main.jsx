c05236bb82df8d1c0b251c3ec195f5e7af7ec3c4
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-26 17:38:41 +0100

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // Import głównego komponentu aplikacji
-import { BrowserRouter } from "react-router-dom"; // Importowanie React Router
-import { Toaster } from "react-hot-toast";
-// Importowanie komponentu Toast do powiadomień
+import App from "./App"; // Import głównego komponentu aplikacji
+import { BrowserRouter } from "react-router-dom"; // Importowanie Routera
 import { CssBaseline, ThemeProvider } from "@mui/material"; // Dla Material UI
 import { createTheme } from "@mui/material/styles"; // Tworzenie niestandardowego motywu
 
@@ -24,15 +23,9 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <BrowserRouter>
-    {" "}
-    {/* Router dla nawigacji po aplikacji */}
     <ThemeProvider theme={theme}>
-      {" "}
-      {/* Dostosowanie tematu Material UI */}
-      <CssBaseline />{" "}
-      {/* Resetowanie stylów, aby aplikacja miała spójną bazę */}
-      <App /> {/* Główny komponent aplikacji */}
-      <Toaster /> {/* Komponent do wyświetlania powiadomień */}
+      <CssBaseline />
+      <App />
     </ThemeProvider>
   </BrowserRouter>
 );
