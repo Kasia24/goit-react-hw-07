fb8b3b013d67e1317f41f3a1256a711fa6bf54e1
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-12-26 17:22:39 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import TasksPage from "./pages/TasksPage";
@@ -6,12 +6,18 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/tasks" element={<TasksPage />} />
-    </Routes>
+    <BrowserRouter>
+      {" "}
+      {/* Komponent BrowserRouter zarządza historią */}
+      <Routes>
+        {" "}
+        {/* Komponent Routes renderuje odpowiednie trasy */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/tasks" element={<TasksPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
